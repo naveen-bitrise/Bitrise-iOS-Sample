8878fafa5b05660e4799c81e76bf2e6bd601bca3
Naveen Nazimudeen <naveen.nazimudeen@bitrise.io>
2025-03-07 10:49:37 -0500

    added screenshots to UITests

--- BitriseTestUITests/BitriseTestUITests.swift
+++ BitriseTestUITests/BitriseTestUITests.swift
@@ -20,12 +20,29 @@ class BitriseTestUITests: XCTestCase {
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        let screenshot = XCUIScreen.main.screenshot()
+        let attachment = XCTAttachment(screenshot: screenshot)
+        attachment.lifetime = .keepAlways
+        attachment.name = "Final State"
+        add(attachment)
+    }
+    
+    // Helper method for taking named screenshots during tests
+    func takeScreenshot(name: String) {
+        let screenshot = XCUIScreen.main.screenshot()
+        let attachment = XCTAttachment(screenshot: screenshot)
+        attachment.name = name
+        attachment.lifetime = .keepAlways
+        add(attachment)
     }
 
     func testExample() throws {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launch()
+        
+        // Take a screenshot after launch
+        takeScreenshot(name: "App Launched")
 
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
@@ -37,6 +54,9 @@ class BitriseTestUITests: XCTestCase {
             measure(metrics: [XCTApplicationLaunchMetric()]) {
                 XCUIApplication().launch()
             }
+            
+            // Take a screenshot after performance measurement
+            takeScreenshot(name: "After Launch Performance Test")
         }
     }
 }
